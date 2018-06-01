468bbaf1ca81ac05e054e7519881ebb562def4bf
Catherine Bostian <catherine.bostian@gmail.com>
2018-06-01 20:14:43 +0200

    add tab and custom ingredient cell

---
+++ pingredients/IngredientTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  IngredientTableViewCell.swift
+//  pingredients
+//
+//  Created by Catherine Bostian on 6/1/18.
+//  Copyright © 2018 Catherine Bostian. All rights reserved.
+//
+
+import UIKit
+
+class IngredientTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
