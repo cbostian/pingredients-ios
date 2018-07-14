d9bb5ffb5c1089d42e7d2800844973ce66943af7
Jimmy Burgess <jimmyburgess@jimmys-air.localdomain>
2018-07-14 13:47:53 -0500

    better refreshing

--- pingredients/RecipesViewController.swift
+++ pingredients/RecipesViewController.swift
@@ -13,10 +13,21 @@ import PinterestSDK
 
 class RecipesViewController : BaseRecipesViewController
 {
+    private let refreshControl = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         recipesViewController = self
+        collectionView?.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(refresh(_:)), for: .valueChanged)
     }
+    
+    @objc private func refresh(_ sender: Any) {
+        cursor = "default"
+        recipes = []
+        loadRecipes()
+    }
+    
     override func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if !isLoadingMore && (scrollView.contentOffset.y +  (scrollView.bounds.height * 2) >= scrollView.contentSize.height) {
             loadRecipes()
@@ -32,6 +43,7 @@ class RecipesViewController : BaseRecipesViewController
         recipes += recipesToAdd
         if recipesToAdd.count == self.recipes.count {
             self.collectionView?.reloadData()
+            self.refreshControl.endRefreshing()
         } else {
             let numberOfItems: [Int] = Array(minNewRecipesIndex...self.recipes.count - 1)
             self.collectionView?.insertItems(at: numberOfItems.map { IndexPath(item: $0, section: 0) })
